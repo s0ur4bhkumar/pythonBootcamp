80742ede9c10e79e350596717c347a0905e3db6b
s0ur4bhkumar <soul65917@gmail.com>
2026-04-19 17:20:40 +0530

    day2 start

---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+# %%
+print("hello world")
+
+# %%
+var = input("enter the number")
+print(var)
+
+# %%
+
+print("hello world \n hello world")
+
+# %%
+
+print("hello, the length of your input is: ", len(input("enter your string: ")))
+
+# %%
+
+print("welcome to the band name generator")
+city = input("what's the name of the city you grew up in: ")
+pet_name = input("what's the name of your pet: ")
+print("your band name is: ", city + " " + pet_name)
---
+++ day2/day2.py
@@ -0,0 +1,15 @@
+# %%
+
+print(type(1244))
+
+# %%
+
+print(type("hello"))
+
+# %%
+
+print(type(True))
+
+# %%
+
+print(type(3.5))
