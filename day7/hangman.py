8af6d8c91f5361a6cc81cc9149af9693aedcb3a7
s0ur4bhkumar <soul65917@gmail.com>
2026-04-21 15:11:27 +0530

    day7 complete

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,23 +1,101 @@
 import random
 
+stages = [
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-
+lives = 6
 
 placeHolder = "".join(["_" for i in chosen_word])
 
-display = ""
-
+game_over = False
+correct_guesses = []
 print(chosen_word)
 print(placeHolder)
 
-guess = input("enter your choice of letter: ").lower()
+while not game_over and lives != 0:
+    guess = input("enter your choice of letter: ").lower()
+    display = ""
 
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guess:
-        display += guess
-    else:
-        display += "_"
+    for i in chosen_word:
+        if i == guess:
+            display += guess
+            correct_guesses.append(guess)
+        elif i in correct_guesses:
+            display += i
+        else:
+            display += "_"
+    print(display)
 
-print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives - 1])
+
+    if "_" not in display:
+        game_over = True
+        print("you win")
+    if lives == 0:
+        print("you lose")
