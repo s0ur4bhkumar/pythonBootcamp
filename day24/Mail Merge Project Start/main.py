8d885984eb6d8efa33d9c80341d3ac9a4ca57b35
s0ur4bhkumar <soul65917@gmail.com>
2026-04-28 20:05:50 +0530

    change file extension to docx

--- day24/Mail Merge Project Start/main.py
+++ day24/Mail Merge Project Start/main.py
@@ -8,5 +8,5 @@ for i in names:
     content[0] = f"Dear {i}".replace("\n", ",\n")
     letter = "".join(content)
     print(type(letter))
-    with open(f"./Output/ReadyToSend/for_{i.strip('\n')}.txt", mode="x+") as file:
+    with open(f"./Output/ReadyToSend/for_{i.strip('\n')}.docx", mode="x+") as file:
         file.write(letter)
