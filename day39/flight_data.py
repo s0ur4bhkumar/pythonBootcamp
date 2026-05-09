0c2110c72ae97a74a8dbeaf8b3f3402854aa1ea5
s0ur4bhkumar <soul65917@gmail.com>
2026-05-09 12:56:03 +0530

    day39 step3 complete(skipped rest)

--- day39/flight_data.py
+++ day39/flight_data.py
@@ -1,3 +1,14 @@
+from flight_search import FlightSearch
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(
+        self, price, origin_airport, destination_airport, out_date, return_date
+    ) -> None:
+        self.price = price
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
+
+    
+
--- day39/main.py
+++ day39/main.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-
+import requests_cache
 from dateutil.relativedelta import relativedelta
 from flight_search import FlightSearch
 from rich import print as prettyPrint
+
+requests_cache.install_cache("flights_data")
 
 today = datetime.now().date()
 six_months_from_now = today + relativedelta(months=6)
@@ -10,7 +12,11 @@ six_months_from_now = today + relativedelta(months=6)
 flights = FlightSearch()
 flights_search_data = flights.check_flights(
     origin_city_code="PNQ",
-    destination_city_code="JFK",
-    from_time="2026-06-10",
-    to_time="2026-06-23",
+    destination_city_code="CDG",
+    from_time=today,
+    to_time=six_months_from_now,
 )
+relevant_flights = (
+    flights_search_data["best_flights"] + flights_search_data["other_flights"]
+)
+prettyPrint(flights_search_data.get('other_flights'))
